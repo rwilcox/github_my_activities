dddc8ee4c08675df39ebab95e30fc56c70c6a73c
Ryan Wilcox <rwilcox@wilcoxd.com>
2017-04-08 12:08:09 -0400

    special handle function for code comments

--- my_activities.rb
+++ my_activities.rb
@@ -11,22 +11,30 @@ module Helpers
   end
 
   def self.handle_PushEvent(current_event)
-
     puts "PUSHed code: <https://github.com/#{current_event.repo.name}>"
     puts "See diff: https://github.com/#{current_event.repo.name}/compare/#{current_event.payload.before}...#{current_event.payload.head}"
   end
+
+
+  def self.handle_CommitCommentEvent(current_event)
+    puts "COMMENTED: #{current_event.payload.comment.html_url} in #{current_event.repo.name}"
+    puts "    Text of comment reads: #{current_event.payload.comment.body}"
+  end
 end
+
 
 def main
   username = ENV["GITHUB_USERNAME"] || `git config github.username`.strip
-  password = ENV["GITHUB_PASSWORD"] || `git config github.password`.strip
+  #password = ENV["GITHUB_PASSWORD"] || `git config github.password`.strip
+  oauth_token = ENV["GITHUB_OAUTH"] || `git config github.oauthtoken`.strip
 
   raise "MUST set GITHUB_USERNAME environmental variable" unless username
-  raise "MUST set GITHUB_PASSWORD environmental variable" unless password
+  #raise "MUST set GITHUB_PASSWORD environmental variable" unless password
+  raise "MUST set GITHUB_OAUTH environmental variable" unless oauth_token
 
-  github = Github.new :login => username, :password => password
+  github = Github.new :oauth_token => oauth_token
 
-  events = github.events.performed username #will return private events also since we are logged in
+  events = github.activity.events.performed username #will return private events also since we are logged in
 
   events.each do |current_event|
    #
